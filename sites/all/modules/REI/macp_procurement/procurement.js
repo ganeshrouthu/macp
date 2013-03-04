bc2e27c66a278d961b4cce5c7c5801b8dacf2a3e
amol.bhavsar <amol.bhavsar@reisystems.in>
2013-03-04 16:03:04 +0530

    Procurement Module Changes: Registration and login form error messages customization.

--- sites/all/modules/REI/macp_procurement/procurement.js
+++ sites/all/modules/REI/macp_procurement/procurement.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function () {
 	//procurement-add-tender
   //edit-upload-upload-button
 	//edit-upload-2-ajax-wrapper
-	
+/*	
 	jQuery("#procurement-add-tender #edit-upload-upload-button").click(function() {
 		jQuery("#edit-upload-2-ajax-wrapper").show();
 	});
@@ -17,7 +17,7 @@ jQuery(document).ready(function () {
 		jQuery("#procurement-add-tender #edit-upload-2-ajax-wrapper").css({display:'block !important'});
 		return false;
 	});
-
+*/
   jQuery(".confirm-delete").click(function() {
     return confirm('Are you sure you want to delete?')
   });
@@ -41,7 +41,11 @@ jQuery(document).ready(function () {
 			email_id: {
         required: "Please enter a valid email address"
       }
-		}
+		},
+		  errorPlacement: function (error, element) {
+				error.insertAfter(element);
+			},
+			errorElement:'span'
   });	
   
   jQuery("#user-register-download-tender").validate({
@@ -79,6 +83,62 @@ jQuery(document).ready(function () {
 				maxlength: 10
       },
     },
+		invalidHandler: function(form, validator) {
+			var errors = validator.numberOfInvalids();
+			if (errors) {
+				jQuery("#errorContainer").css('color', 'red');
+				var message	=	'Highlighted fields are mandatory.';
+				var email_id = jQuery.trim(jQuery("#edit-email-id").val());
+				var new_pass = jQuery.trim(jQuery("#edit-new-pass").val());
+				var confirm_passord = jQuery.trim(jQuery("#edit-confirm-passord").val());
+				var agency_name = jQuery.trim(jQuery("#edit-agency-name").val());
+				var person_name = jQuery.trim(jQuery("#edit-person-name").val());
+				var cont_number = jQuery.trim(jQuery("#edit-cont-number").val());
+				var mobile_number = jQuery.trim(jQuery("#edit-mobile-number").val());
+
+				if (!valid_email(email_id)) {
+					message = 'Please enter valid Email ID.';
+				}
+				else if (new_pass.length < 5) {
+					message = 'Password should be at least 5 characters long.';
+				}
+				else if (confirm_passord.length < 5) {
+					message = 'Confirm Password should be at least 5 characters long.';
+				}
+				else if (new_pass != confirm_passord) {
+					message = 'Password and Confirm Password should be same.';
+				}
+				else if (agency_name.length > 0 && agency_name.length < 5) {
+					message = 'Agency Name should be at least 5 characters long.';
+				}
+				else if (person_name.length > 0 && person_name.length < 5) {
+					message = 'Person Name should be at least 5 characters long.';
+				}
+				else if (cont_number.length > 0 && cont_number.length < 5) {
+					message = 'Contact Number should be at least 5 digits long.';
+				}
+				else if (mobile_number.length > 0 && mobile_number.length < 5) {
+					message = 'Mobile Number should be at least 5 digits long.';
+				}
+				else if (agency_name.length > 0 && agency_name.length > 10) {
+					message = 'Agency Name should be more than 10 characters long.';
+				}
+				else if (person_name.length > 0 && person_name.length > 10) {
+					message = 'Person Name should not be more than 10 characters long.';
+				}
+				else if (cont_number.length > 0 && cont_number.length > 10) {
+					message = 'Contact Number should not be more than 10 digits long.';
+				}
+				else if (mobile_number.length > 0 && mobile_number.length > 10) {
+					message = 'Mobile Number should not be more than 10 digits long.';
+				}
+				jQuery("#errorContainer").html(message);
+			}
+		},
+		errorPlacement: function(error, element) {
+			return true;
+		},
+		
     messages: {
     	email_id: "Please enter a valid email address",
       pass: {
@@ -141,3 +201,8 @@ jQuery(document).ready(function () {
 		//alert(url_to);
 	});
 })
+
+function valid_email(email) {
+	var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email);
+}
